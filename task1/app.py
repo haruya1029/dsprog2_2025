c85dfc4f065df60ca6f269a46817770be3fa32ad
haruya1029 <s2522009@stu.musashino-u.ac.jp>
2026-01-06 16:27:01 +0900

    課題天気予報修正

--- task1/app.py
+++ task1/app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import requests
 
-# =========================
-# タイトル
-# =========================
-st.title("☀️ 気象庁 天気予報アプリ")
 
-# =========================
+# タイトル
+
+st.title("気象庁 天気予報アプリ")
+
+
 # 地域リスト取得
-# =========================
+
 AREA_URL = "http://www.jma.go.jp/bosai/common/const/area.json"
 area_data = requests.get(AREA_URL).json()
 
@@ -18,9 +18,8 @@ areas = area_data["offices"]
 # 表示用データ作成
 area_names = {v["name"]: k for k, v in areas.items()}
 
-# =========================
 # 地域選択
-# =========================
+
 selected_area_name = st.selectbox(
     "地域を選択してください",
     area_names.keys()
@@ -28,16 +27,16 @@ selected_area_name = st.selectbox(
 
 area_code = area_names[selected_area_name]
 
-# =========================
+
 # 天気予報取得
-# =========================
+
 FORECAST_URL = f"https://www.jma.go.jp/bosai/forecast/data/forecast/{area_code}.json"
 forecast_data = requests.get(FORECAST_URL).json()
 
-# =========================
+
 # 天気予報表示
-# =========================
-st.subheader(f"📍 {selected_area_name} の天気予報")
+
+st.subheader(f" {selected_area_name} の天気予報")
 
 time_series = forecast_data[0]["timeSeries"][0]
 dates = time_series["timeDefines"]
